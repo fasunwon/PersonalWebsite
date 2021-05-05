a04d1b0119d4994ef9c97236c1518f401783178f
Favor <ffp638@uregina.ca>
2021-05-04 18:17:04 -0600

    rendering elements

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import Navbar from '../components/navbar';
+import SideBar from '../components/SideBar';
+import {homeObjOne, homeObjThree, homeObjFour} from '../components/InfoSection/Data';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
+const Home = () => {
+    const [isOpen, setIsOpen]= useState(false);
+
+    const toggle = () =>{
+        setIsOpen(!isOpen);
+    }
+
+
+    return (
+        <>
+        <SideBar isOpen={isOpen} toggle={toggle}/>
+        <Navbar toggle={toggle}/>
+        <HeroSection/>
+        <InfoSection {...homeObjOne} style={{Button:"hidden"}}/>
+        <Projects/>
+        <InfoSection {...homeObjThree}/>
+        <InfoSection {...homeObjFour}/>
+        <Footer/>
+        </>
+    )
+}
+
+export default Home
