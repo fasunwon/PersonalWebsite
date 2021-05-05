06693d2fb9aac88406db3504e7168005faf42be5
Favor <ffp638@uregina.ca>
2021-05-04 18:07:05 -0600

    Data keys

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,60 @@
+export const homeObjOne = {
+    id:'about',
+    LightBg: true,
+    LightText: true,
+    LightTextDesc: true,
+    topLine: 'Introduction',
+    headLine: 'Software Systems Engineer',
+    description: `Hi My name is Favor Temiloluwa Fasunwon and i am currently in my 3rd year of Software Systems Engineering
+    at the University of Regina. I am developing a passion for web dev, and will 
+    continue advancing in this field because it makes me happy 😊. I love to learn and 
+    challenge myself, even if it means failing at times to succeed. 
+    I work hard and continue to evolve my skills as a Web developer. `,
+    // buttonLabel:`Let's connect`,
+    imgStart:true,
+    img: require('../../img/favor.svg').default,
+    alt: 'person',
+    dark:true,
+    primary:true,
+    darkText: false
+};
+
+export const homeObjThree = {
+    id:'interests',
+    LightBg: true,
+    LightText: true,
+    LightTextDesc: true,
+    topLine: 'Interests',
+    headLine: 'Coding 😁',
+    description: `As you can tell by the Emoji, i love to code. More so, i have interests
+    that lie outside of coding, such as Playing the Saxophone🎷, 
+    playing basketball 🏀, Watching the stars at night 🌟, and creating christian rap music 😎.`,
+    buttonLabel:`Let's connect`,
+    imgStart:true,
+    img: require('../../img/coding.svg').default,
+    alt: 'music',
+    dark:true,
+    primary:true,
+    darkText: false
+};
+
+export const homeObjFour = {
+    id:'belief',
+    LightBg: false,
+    LightText: false,
+    LightTextDesc: true,
+    topLine: 'My Beliefs',
+    headLine: 'Phillipians 4:13',
+    description: `"I can do all things through christ who Strengthens me". My faith
+    in christ is unwavering, from a young age i was introducted to the bible and its teachings.
+    The bible has taught me many things, an important concept being to act morally
+    ethical and Love your neighbour as you love yourself. I incorporate these concepts
+    in everything i do, and yes that also includes when i code!! `,
+    buttonLabel:`Check verse `,
+    imgStart:true,
+    img: require('../../img/belief.svg').default,
+    alt: 'belief',
+    dark:true,
+    primary:false,
+    darkText: false
+};
