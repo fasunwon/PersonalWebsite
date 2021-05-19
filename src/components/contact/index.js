eefef8abbcd0ac5aa82e42656d576d24fc6b5e5a
Favor <ffp638@uregina.ca>
2021-05-19 16:40:51 -0600

    Updated

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Container, FormContent, FormWrap, Icon,
-Form, FormH1, FormLabel, Picture } from './contactElements'
+Form, FormH1, FormLabel,FormMessage, FormInput, FormButton} from './contactElements'
 import Img from '../../img/HomeLogo.png';
-import Img2 from '../../img/contact.svg';
+// import Img2 from '../../img/contact.svg';
 import {ImageBg} from '../navbar/NavbarElements';
 const Contact = () => {
     return (
@@ -13,19 +13,17 @@ const Contact = () => {
                 <ImageBg src={Img} type='image/png'/>
                 </Icon>
                 <FormContent>
-                    <Form action="#">
-                    <Picture src={Img2} type = 'image/svg'/>
-                    <FormH1>Ways to contact me!!</FormH1>
-                    <FormLabel htmlFor='for'>Email:
-                    favor_fasunwon21@hotmail.com</FormLabel>
-                    <br></br>
-                    <br></br>
-                    <FormLabel htmlFor='for'>Phone Number:
-                    (306)501-9442</FormLabel>
-                    <br></br>
-                    <br></br>
-                    <FormLabel htmlFor='for'>Mail:
-                    1445 Royal St Regina, SK S4T5A4</FormLabel>
+                    <Form action="https://formsubmit.co/favor_fasunwon21@hotmail.com" method="POST">
+                    {/* <Picture src={Img2} type = 'image/svg'/> */}
+                    <FormH1>Contact Me!!</FormH1>
+                    <FormLabel htmlFor='for'>Name</FormLabel><br></br>
+                        <FormInput type='text' placeholder="First and Last name" required /><br></br>
+                        <FormInput type='hidden' name="_subject" value="You got Mail!" />
+                    <FormLabel htmlFor='for'>Email</FormLabel><br></br>
+                        <FormInput type='email' placeholder="Email address....." required /><br></br>
+                    <FormLabel htmlFor='for'>Message</FormLabel><br></br>
+                        <FormMessage type='text' placeholder="Message....." required /><br></br>
+                    <FormButton type='submit'>Send ✈</FormButton>
                     </Form>
                 </FormContent>
             </FormWrap>
